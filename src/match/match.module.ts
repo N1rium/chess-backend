4b97d1f541d1ee888cd131ca1203a7aa6887630a
blomman <johnny@gumbler.com>
2020-04-16 20:17:09 +0200

    Added MatchParticipant to match module

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MatchResolver } from './match.resolver';
 import { MatchService } from './match.service';
-import { Match, MatchMove } from './match.entity';
+import { Match, MatchMove, MatchParticipant } from './match.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match, MatchMove])],
+  imports: [TypeOrmModule.forFeature([Match, MatchMove, MatchParticipant])],
   providers: [MatchResolver, MatchService],
 })
 export class MatchModule {}
