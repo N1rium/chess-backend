789d7cbac8d2138d7c9bbf8525f2c297c7b944db
blomman <johnny@gumbler.com>
2020-04-16 20:12:03 +0200

    Login stuff for user

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -12,29 +12,29 @@ import {
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  @Query(_returns => User, { name: 'userById' })
+  @Query(() => User, { name: 'userById' })
   userById(@Args('id') id: string): Promise<User> {
     return this.userService.userById(id);
   }
 
-  @Query(_returns => User, { name: 'userByEmail' })
+  @Query(() => User, { name: 'userByEmail' })
   userByEmail(@Args('email') email: string): Promise<User> {
     return this.userService.userByEmail(email);
   }
 
-  @Query(_returns => User, { name: 'me' })
+  @Query(() => User, { name: 'me' })
   me(@Context() ctx): Promise<User> {
     return this.userService.me(ctx.token);
   }
 
-  @Query(_returns => LoginResponse, { name: 'login' })
+  @Query(() => LoginResponse, { name: 'login' })
   login(
     @Args('input', { type: () => LoginInput }) input: LoginInput,
   ): Promise<LoginResponse> {
     return this.userService.login(input);
   }
 
-  @Mutation(_returns => LoginResponse)
+  @Mutation(() => LoginResponse)
   createUser(
     @Args('input', { type: () => CreateUserInput }) input: CreateUserInput,
   ): Promise<LoginResponse> {
