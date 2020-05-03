2cd5f6b2ba04871e0862cf818938ddd2850ec7c8
matt lotfi <matt@lotfi.se>
2020-05-03 23:50:12 +0200

    added playground

--- src/graphql.module.ts
+++ src/graphql.module.ts
@@ -1,8 +1,10 @@
 import { GraphQLModule } from '@nestjs/graphql';
 
 export default GraphQLModule.forRoot({
-  autoSchemaFile: 'src/schema.gql',
+  autoSchemaFile: 'tmp/schema.gql',
   installSubscriptionHandlers: true,
+  playground:true,
+  introspection: true,
   context: async ({ req }: any) => {
     const ctx = {
       ...(req?.headers?.token && { token: req.headers.token }),
